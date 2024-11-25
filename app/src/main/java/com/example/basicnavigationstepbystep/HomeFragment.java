f05ddfe450101da02ad0787d7d392d68e2ad67ab
Ricardo <ricardo.orzu@gmail.com>
2024-11-25 02:53:00 -0300

    Initial commit version 1.0

---
+++ app/src/main/java/com/example/basicnavigationstepbystep/HomeFragment.java
@@ -0,0 +1,99 @@
+package com.example.basicnavigationstepbystep;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentController;
+import androidx.navigation.NavController;
+import androidx.navigation.NavOptions;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+
+public class HomeFragment extends Fragment {
+
+    private Fragment thisFragment=this;
+
+    private NavController thisNavController;
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
+        return inflater.inflate(R.layout.home_fragment, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        thisNavController = NavHostFragment.findNavController(thisFragment);
+
+        //TODO STEP 2 - Set an OnClickListener, using Navigation.createNavigateOnClickListener()
+        Button navigateButton = (Button) getView().findViewById(R.id.navigate_destination_button);
+        //Comentamos la siguiente línea como paso previo del STEP 3
+        //navigateButton.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.flow_step_one_dest,null));
+        //END STEP 2
+
+        //TODO STEP 3 - Set NavOptions
+        //Creamos el objeto navigation Builder y utilizamos los métodos para cargar las animaciones.
+        NavOptions.Builder navOptionsBuilder = new NavOptions.Builder();
+        navOptionsBuilder.setEnterAnim(R.anim.slide_in_right);
+        navOptionsBuilder.setExitAnim(R.anim.slide_out_left);
+        navOptionsBuilder.setPopExitAnim(R.anim.slide_out_right);
+        navOptionsBuilder.setPopEnterAnim(R.anim.slide_in_left);
+
+        //Creamos el objeto NavOptions
+        final NavOptions options = navOptionsBuilder.build();
+
+        //En el navigateButton del STEP2 le colocamos el listener aquí, por eso comentamos el de allí.
+        navigateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(thisFragment).navigate(R.id.flow_step_one_dest,null,options);
+            }
+        });
+
+        //END STEP 3
+
+        //TODO STEP 4 - OnClickListener to navigate using an action
+//        Button actionButton = (Button) view.findViewById(R.id.navigate_action_button);
+//        actionButton.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.next_action,null));
+        //END STEP 4
+
+        //TODO STEP 7 - Update the OnClickListener to navigate using an action and using  ...Direction clases for arguments
+        Button actionButton = (Button) getView().findViewById(R.id.navigate_action_button);
+        actionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Es el primer elemento en el flujo de navegación, le pongo el valor 1
+                int flowStepNumber=1;
+
+                //Creamos un objeto action de tipo HomeFragmentDirections.NextAction
+                HomeFragmentDirections.NextAction action = HomeFragmentDirections.nextAction();
+
+                //Llamo al metoto que setea el argumento y le paso el valor.
+                action.setFlowStepNumber(flowStepNumber);
+
+                //Ejecuto la navegación con el método navigate
+                //Lo puedo hacer de esta manera
+                    //NavHostFragment.findNavController(thisFragment).navigate(action);
+                //O de esta, que escribo menos código, necesito crear el thisFragmentController arriba.
+                    thisNavController.navigate(action);
+            }
+        });
+        //END STEP 7
+
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.main_menu, menu);
+    }
+}
